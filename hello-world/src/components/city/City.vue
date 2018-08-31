878d9bc98aeb445005a6c56d3fb4f028036a6d42
weizb <wzb19960208@outlook.com>
2018-08-31 14:41:24 +0800

    '添加了城市数据的JSON，完成了模糊搜索功能，并能够选择在搜索下的结果并设置为当前城市以及回到首页'

--- hello-world/src/components/city/City.vue
+++ hello-world/src/components/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :city="city"></city-search>
     <city-list></city-list>
   </div>
 </template>
@@ -10,9 +10,30 @@
   import CityHeader from './components/Header'
   import CitySearch from "./components/Search"
   import CityList from "./components/List";
+  import axios from 'axios'
 export default {
   name:'City',
-  components: {CityList, CitySearch, CityHeader}
+  components: {CityList, CitySearch, CityHeader},
+  data(){
+    return{
+      city : ''
+    }
+  },
+  methods:{
+    getCityInfo(){
+      axios.get('/static/mock/city.json').then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc(res){
+      res = res .data
+      if(res.ret && res.data){
+        this.city = res.data;
+      }
+
+    }
+  },
+  mounted(){
+    this.getCityInfo();
+  }
 }
 </script>
 
--- hello-world/src/components/city/components/Search.vue
+++ hello-world/src/components/city/components/Search.vue
@@ -1,13 +1,67 @@
 <template>
-  <div class="wrapper">
-    <input class="search" type="text" placeholder="输入城市名或拼音"/>
-  </div>
+
+      <div class="wrapper">
+        <input class="search" v-model="content" type="text" placeholder="输入城市名或拼音"/>
+        <div class="search-content" v-show="content">
+          <ul>
+            <li class="search-item"
+                v-for="item of allCity"
+                :key="item.id"
+                @click="handleClick"
+            >{{item.name}}</li>
+          </ul>
+        </div>
+
+      </div>
+
 </template>
 
 
 <script>
 export default {
-  name : 'CitySearch'
+  name : 'CitySearch',
+  props:['city'],
+  data(){
+    return{
+      content:'',
+      allCity : []
+    }
+  },
+  methods:{
+    handleClick(event){
+      var city = event.srcElement.innerHTML;
+      this.$store.dispatch('changeCity',city);
+      this.$router.push('/');
+    }
+  },
+  watch :{
+    // 对content属性进行监听
+    content (){
+
+      var result = [];
+
+      for(let i of this.city.hot){
+        if(i.name.indexOf(this.content)>-1 || i.spell.indexOf(this.content)>-1){
+          result.push(i)
+        }
+      }
+
+      for(let i of this.city.all){
+        for(let j of i.city){
+          if(j.name.indexOf(this.content)>-1 || j.spell.indexOf(this.content)>-1){
+            result.push(j)
+          }
+        }
+      }
+
+      if (result.length === 0) {
+        result.push({id:'999',name:'没有找到匹配数据'})
+      }
+
+      this.allCity = result
+
+    }
+  }
 }
 </script>
 
@@ -15,6 +69,7 @@ export default {
 <style scoped>
 
   .wrapper{
+    position: relative;
     height: .72rem;
     background: #00bcd4;
   }
@@ -28,5 +83,21 @@ export default {
     padding-left: .1rem;
     padding-right: .1rem;
   }
+
+  .search-content{
+    width: 100%;
+    position: absolute;
+    top: .72rem;
+    background: #fff;
+  }
+
+  .search-item{
+    width: 100%;
+    line-height: .76rem;
+    padding-left: .2rem;
+    border-bottom: #eee solid 1px;
+
+  }
+
 
 </style>
